8a22e98bf3ba327ead6d7c61b41e9293b934bef0
Eliel Souza <eliel.souzha@gmail.com>
2022-12-14 23:27:20 -0300

    add for loop

--- scripts/convert-jpg-png.sh
+++ scripts/convert-jpg-png.sh
@@ -2,5 +2,8 @@
 
 path_image=~/shell-scripting1/imagens-livros
 
-convert $path_image/$1.jpg $path_image/$1.png
-convert $path_image/$2.jpg $path_image/$2.png
+for image in $@; do
+	
+	convert $path_image/$image.jpg $path_image/$image.png
+done
+
